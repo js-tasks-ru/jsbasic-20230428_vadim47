24dff0505bf5151d834696fa9b9a885523cc9835
GDhsXAzV <102017170+GDhsXAzV@users.noreply.github.com>
2023-05-02 17:46:41 +0300

    First task

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,16 @@
 function sum(m, n) {
   // ваш код...
+  
+  let currentSum = a;
+
+  function f(b) {
+    currentSum += b;
+    return f;
+  }
+
+  f.toString = function() {
+    return currentSum;
+  };
+
+  return f;
 }
