47401260eb06e6703ceb4f0c819b745a05b24ebf
GDhsXAzV <102017170+GDhsXAzV@users.noreply.github.com>
2023-05-16 09:22:53 +0300

    Module 0-3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
   // ваш код...
+  let res = 0;
+  
+  for (let unit in salaries) {
+
+    let unitValue = salaries[unit];
+
+    if (Number.isFinite (unitValue)) {
+      res = res + unitValue;
+    }
+  }
+  return (res);
+  
 }
