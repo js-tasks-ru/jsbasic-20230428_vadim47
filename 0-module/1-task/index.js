7c78a5b8b8d9c24723d5616da328174a740e2169
GDhsXAzV <102017170+GDhsXAzV@users.noreply.github.com>
2023-05-16 09:21:05 +0300

    Module 0-5

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,16 +1,5 @@
 function sum(m, n) {
   // ваш код...
   
-  let currentSum = a;
-
-  function f(b) {
-    currentSum += b;
-    return f;
-  }
-
-  f.toString = function() {
-    return currentSum;
-  };
-
-  return f;
+  return m + n;
 }
